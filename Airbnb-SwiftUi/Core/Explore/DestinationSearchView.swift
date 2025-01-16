2dfc51b27dd8d67130e4dbef229911ce6c5ecc46
kenan551 <kenan.ramazanov1104@gmail.com>
2025-01-16 23:04:14 +0400

    Creating SearchView

---
+++ Airbnb-SwiftUi/Core/Explore/DestinationSearchView.swift
@@ -0,0 +1,20 @@
+//
+//  DestinationSearchView.swift
+//  Airbnb-SwiftUi
+//
+//  Created by Kanan  on 16.01.25.
+//
+
+import SwiftUI
+
+struct DestinationSearchView: View {
+    var body: some View {
+        VStack {
+            Text("Where to?")
+        }
+    }
+}
+
+#Preview {
+    DestinationSearchView()
+}
