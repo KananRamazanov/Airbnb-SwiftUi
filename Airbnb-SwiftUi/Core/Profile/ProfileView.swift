6f2856f1f44d1bb4305b3a0bfdd97ed5ec8c22a2
kenan551 <kenan.ramazanov1104@gmail.com>
2025-01-31 23:06:04 +0400

    Creating Profile View

---
+++ Airbnb-SwiftUi/Core/Profile/ProfileView.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileView.swift
+//  Airbnb-SwiftUi
+//
+//  Created by Kanan  on 31.01.25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text("Profile")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            
+            
+            Text("Log in to start planning your next trip")
+            
+        }
+    }
+}
+
+#Preview {
+    ProfileView()
+}
