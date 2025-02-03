89b21133139f93e46f220b502a2b3032bbf62160
kenan551 <kenan.ramazanov1104@gmail.com>
2025-02-03 20:14:33 +0400

    Editing Profile View

--- Airbnb-SwiftUi/Core/Profile/ProfileView.swift
+++ Airbnb-SwiftUi/Core/Profile/ProfileView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
-            Text("Profile")
-                .font(.largeTitle)
-                .fontWeight(.semibold)
+        VStack(alignment: .leading, spacing: 32) {
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Profile")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                
+                Text("Log in to start planning your next trip")
+            }
             
             
-            Text("Log in to start planning your next trip")
+            
+            Button {
+                print("Log in")
+            } label: {
+                Text("Log in")
+                    .foregroundStyle(.white)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .frame(width: 360, height: 48)
+                    .background(.pink)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+            }
+            HStack {
+                Text("Don't have an account?")
+                
+                Text("Sign up")
+                    .fontWeight(.semibold)
+                    .underline()
+            }
+            .font(.caption)
             
         }
     }
